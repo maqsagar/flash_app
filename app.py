1a56997f2046e73744e9a2752ea2856cae8df1b5
[Maqsood Ali] <[maqsagar@gmail.com]>
2025-11-05 20:13:20 +0530

    reupdate all data with pymongo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-#test
+#removed
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Required for flash messages
 
